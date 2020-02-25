69f0bf8b2b90d87de85b902eb490086d60569ee7
Sega <057sega057@gmail.com>
2020-02-25 20:14:34 +0200

    Deploy

---
+++ docs/precache-manifest.78ac735417a20f3e3e5909e1d94410af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b153ddc18de527b445a2a25d65cee1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44d4e339a559ee0e27e3",
+    "url": "/static/css/main.6b35ca4c.chunk.css"
+  },
+  {
+    "revision": "e7b51e8b3231f4f8198d",
+    "url": "/static/js/2.c150a1b8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.c150a1b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44d4e339a559ee0e27e3",
+    "url": "/static/js/main.af7692cf.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  }
+]);
